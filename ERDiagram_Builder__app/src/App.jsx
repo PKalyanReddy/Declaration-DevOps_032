60ec2f2ad36a388bb78c9c0054ff80adbbe6fe97
Ritik26052000 <sonwanianshu2000@gmail.com>
2024-07-18 00:04:11 +0530

    ft33_227_day2_whiteBoard_created

--- ERDiagram_Builder__app/src/App.jsx
+++ ERDiagram_Builder__app/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import WhiteSpace from "./components/WhiteSpace/WhiteSpace";
 
 function App() {
-
   return (
     <>
-      
+      <h1>App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
